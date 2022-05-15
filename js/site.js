8abcc80c8cbda8257c3346811a1de31986684a67
Makkenzie <makkenzie1@ymail.com>
2022-05-15 12:32:21 -0700

    added notes and edited js

--- js/site.js
+++ js/site.js
@@ -1,16 +1,34 @@
 
 // updates htlm visability for songs being selected
-function displayMoods(moodNum) {
+function displayMoods() {
   If (moodNum == 1)
   {
-     //elements you want to show as visible
-   document.getElementById("song1").style.display="visible";
+
+  //elements you want to show as visible
+  document.getElementById("mood1").style.visibility ="visible";
 
   //don't forget to set all the other elements to hidden
-  document.getElementById("song2").style.display="visible";
-  document.getElementById("song3").style.display="visible";
-  document.getElementById("song4").style.display="visible";
-  document.getElementById("song5").style.display="visible";
+  document.getElementById("mood2").style.visibility ="visible";
+  document.getElementById("mood3").style.visibility ="visible";
+  document.getElementById("mood4").style.visibility ="visible";
+  document.getElementById("mood5").style.visibility ="visible";
 
   }
 }
+
+
+//Each mood has five songs connected to it
+//EX: Mood 1 = 5 songs ==> Each song has one fact bar
+
+//Would I call .innerHTML to call upon my displayMoods ??
+//EX: <p id="demo"></p>
+// <script>
+// const myObject = {
+//   firstName:"John",
+//   lastName: "Doe",
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// document.getElementById("demo").innerHTML = myObject.fullName();
+// </script>
