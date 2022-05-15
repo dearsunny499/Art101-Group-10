099fbb9fdbb67e063a823e573b559e4ff5fdc612
Makkenzie <makkenzie1@ymail.com>
2022-05-15 11:03:40 -0700

    edited html and css

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 
 // updates htlm visability for songs being selected
-function displaySongs(moodNum) {
+function displayMoods(moodNum) {
   If (moodNum == 1)
   {
      //elements you want to show as visible
-   document.getElementById("Popup").style.display="visible";
+   document.getElementById("song1").style.display="visible";
 
   //don't forget to set all the other elements to hidden
-  document.getElementById("song1").style.display="hidden";
-  document.getElementById("song2").style.display="hidden";
-  document.getElementById("song3").style.display="hidden";
-  document.getElementById("song4").style.display="hidden";
-  document.getElementById("song5").style.display="hidden";
+  document.getElementById("song2").style.display="visible";
+  document.getElementById("song3").style.display="visible";
+  document.getElementById("song4").style.display="visible";
+  document.getElementById("song5").style.display="visible";
+
   }
 }
