d54f7e2930b9d97c949fdf74a1568d197c21f0d2
Makkenzie <makkenzie1@ymail.com>
2022-05-18 08:47:52 -0700

    edited css, js, and html
    got the mood buttons to display but not in their containers

--- js/site.js
+++ js/site.js
@@ -11,18 +11,9 @@ $("#main-btn").click(displayMoods);
 
 
 
+
+
+
+
 //Each mood has five songs connected to it
 //EX: Mood 1 = 5 songs ==> Each song has one fact bar
-
-//Would I call .innerHTML to call upon my displayMoods ??
-//EX: <p id="demo"></p>
-// <script>
-// const myObject = {
-//   firstName:"John",
-//   lastName: "Doe",
-//   fullName: function() {
-//     return this.firstName + " " + this.lastName;
-//   }
-// }
-// document.getElementById("demo").innerHTML = myObject.fullName();
-// </script>
