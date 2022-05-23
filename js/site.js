aab9296cc1a89805b5ccb0030f2b726d0a7a26f9
Makkenzie <makkenzie1@ymail.com>
2022-05-23 14:30:36 -0700

    added spotify playlist to the footer of anxiety

--- js/site.js
+++ js/site.js
@@ -217,6 +217,7 @@ $(".song").click(function(){
     <div class="desc">${songData.description03}</div>
 
     <img class="image" src='img/${songData.image}'>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EFm15MFG2FKNZ?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 
 </body>
 </html>
@@ -345,6 +346,7 @@ $(".song").click(function(){
     <div class="desc">${songData.description03}</div>
 
     <img class="image" src='img/${songData.image}'>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EFm15MFG2FKNZ?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 
 </body>
 </html>
